1d9612ae04851c58f8f90ec80678f9a78890f108
fernando <fdoidera@gmail.com>
2021-10-04 08:53:36 -0300

    Diminuindo tempo para expirar o token

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -63,7 +63,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 3600
+            'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
     }
 }
