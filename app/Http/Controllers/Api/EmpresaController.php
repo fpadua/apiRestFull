376aa2348e572e9aec1142f688e6e7808c9bf424
fernando <fdoidera@gmail.com>
2021-10-04 12:34:41 -0300

    Refatorando codigo da index

--- app/Http/Controllers/Api/EmpresaController.php
+++ app/Http/Controllers/Api/EmpresaController.php
@@ -22,45 +22,7 @@ class EmpresaController extends Controller
      */
     public function index(Request $request)
     {
-        $empresas = Empresa::query();
-
-        if ($request->exists('cpf_cnpj')) {
-            $empresas->where('cpf_cnpj', $request->query('cpf_cnpj'));
-        }
-
-        if ($request->exists('razao_social')) {
-            $empresas->where('razao_social', 'like', "%{$request->query('razao_social')}%");
-        }
-
-        if ($request->exists('nome_fantasia')) {
-            $empresas->where('nome_fantasia', 'like', "%{$request->query('nome_fantasia')}%");
-        }
-
-        if ($request->exists('tipo_pessoa')) {
-            $empresas->where('tipo_pessoa', $request->query('tipo_pessoa'));
-        }
-
-        if ($request->exists('tipo_contribuinte')) {
-            $empresas->where('tipo_contribuinte', $request->query('tipo_contribuinte'));
-        }
-
-        if ($request->exists('tipo_cadastro')) {
-            $empresas->where('tipo_cadastro', $request->query('tipo_cadastro'));
-        }
-
-        if ($request->exists('estado')) {
-            $empresas->where('estado', $request->query('estado'));
-        }
-
-        if ($request->exists('cidade')) {
-            $empresas->where('cidade', 'like', "%{$request->query('cidade')}%");
-        }
-
-        if ($request->exists('pais')) {
-            $empresas->where('pais', $request->query('pais'));
-        }
-
-        $empresas = $empresas->paginate(10);
+        $empresas = Empresa::search($request);
         return EmpresaResource::collection($empresas);
     }
 
